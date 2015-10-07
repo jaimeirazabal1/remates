0ebd0f8faf934a75c4037f442d67598a2de1e55d
jaimeirazabal1 <jaimeirazabal1@gmail.com>
2015-10-07 16:09:52 -0430

    first commit

---
+++ default/app/libs/app_controller.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @see Controller nuevo controller
+ */
+require_once CORE_PATH . 'kumbia/controller.php';
+
+/**
+ * Controlador principal que heredan los controladores
+ *
+ * Todas las controladores heredan de esta clase en un nivel superior
+ * por lo tanto los metodos aqui definidos estan disponibles para
+ * cualquier controlador.
+ *
+ * @category Kumbia
+ * @package Controller
+ */
+class AppController extends Controller
+{
+
+    final protected function initialize()
+    {
+    	$this->title_principal = "Remates";
+    }
+
+    final protected function finalize()
+    {
+        
+    }
+
+}
